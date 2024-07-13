264e5d15e6833441452a3b0413a357f732fcf056
koleso8 <dev.koleso@gmail.com>
2024-07-13 14:29:53 +0300

    qwe

--- vite.config.js
+++ vite.config.js
@@ -20,7 +20,7 @@ export default defineConfig(({ command }) => {
               return 'vendor';
             }
           },
-          // entryFileNames: 'commonHelpers.js',
+          entryFileNames: 'commonHelpers.js',
         },
       },
       outDir: '../dist',
