35aeff76ee199d78e718cdd50b2b2a8515d81223
koleso8 <dev.koleso@gmail.com>
2024-07-16 12:21:23 +0300

    q

--- src/main.js
+++ src/main.js
@@ -2,13 +2,17 @@ const refs = {
   users: document.querySelector('.users'),
 };
 
-for (let i = 0; i < 50; i++) {
-  fetch('https://jsonplaceholder.typicode.com/todos/')
-    .then(response => response.json())
-    .then(json => {
-      const user = json[i];
-      const renderUser = `<p>id: ${user.id}<br>title: ${user.title}<br>completed: ${user.completed}</p><br>`;
-      console.log(renderUser);
-      refs.users.insertAdjacentHTML('beforeend', renderUser);
-    });
-}
+import axios from 'axios';
+
+axios
+  .get('https://jsonplaceholder.typicode.com/users')
+  .then(response => {
+    console.log(response.data);
+    console.log(response.status);
+    console.log(response.statusText);
+    console.log(response.headers);
+    console.log(response.config);
+  })
+  .catch(error => {
+    console.log(error);
+  });
