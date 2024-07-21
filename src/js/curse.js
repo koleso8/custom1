50e53b306750d452bed13cca87f48351b1d8d106
koleso8 <dev.koleso@gmail.com>
2024-07-21 11:47:54 +0300

    q

--- src/js/curse.js
+++ src/js/curse.js
@@ -1,17 +1,12 @@
-import axios from 'axios';
+// core version + navigation, pagination modules:
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/autoplay';
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+  crossFade: true,
+  effect: 'coverflow',
+});
 
-const options = {
-  method: 'GET',
-  url: 'https://binance43.p.rapidapi.com/ticker/24hr',
-  headers: {
-    'x-rapidapi-key': 'bbe53b8be3msh36fe641fd2bce6ep18f39bjsne3eca22963e1',
-    'x-rapidapi-host': 'binance43.p.rapidapi.com',
-  },
-};
-
-try {
-  const response = await axios.request(options);
-  console.log(response.data);
-} catch (error) {
-  console.error(error);
-}
+console.log(swiper);
