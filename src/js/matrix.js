a42ea0813eb9a5a82ecc87bc4c4889da64533172
koleso8 <dev.koleso@gmail.com>
2024-07-31 21:42:13 +0300

    add matrix

--- src/js/matrix.js
+++ src/js/matrix.js
@@ -1,38 +1,49 @@
-/*
-<canvas id="q" width="1370" height="400">
-      Sorry Browser Won't Support
-</canvas>
-*/
+const C = document.querySelector('canvas'),
+  $ = C.getContext('2d'),
+  W = (C.width = innerWidth),
+  H = (C.height = innerHeight);
 
-let s = window.screen;
-let width = 1370;
-let height = 400;
-let yPositions = Array(300).join(0).split('');
-let ctx = q.getContext('2d');
-let draw = function () {
-  ctx.fillStyle = 'rgba(0,0,0,.1)';
-  ctx.fillRect(0, 0, width, height);
-  ctx.fillStyle = '#0F0';
-  ctx.font = '10pt Georgia';
-  yPositions.map(function (y, index) {
-    text = String.fromCharCode(1e2 + Math.random() * 33);
-    x = index * 10 + 10;
-    q.getContext('2d').fillText(text, x, y);
-    if (y > 100 + Math.random() * 1e4) {
-      yPositions[index] = 0;
-    } else {
-      yPositions[index] = y + 10;
-    }
-  });
-};
-RunMatrix();
+const str =
+    'А+Б0В-Г1Д=Е2Ё Ж3Зaasdasdasdasdasdas И4Й К5Л М6Н О7П Р8С Т9У Ф!Х Ц?Ч Ш.ЩЪ,Ы Ь:ЭЮ;Я',
+  matrix = str.split('');
 
-function RunMatrix() {
-  if (typeof Game_Interval != 'undefined') clearInterval(Game_Interval);
-  Game_Interval = setInterval(draw, 70);
+let font = 11,
+  // количество колонок = ширина холста / размер шрифта
+  col = W / font,
+  arr = [];
+
+for (let i = 0; i < col; i++) arr[i] = 1;
+
+function draw() {
+  // определяем цвет фона
+  // такой цвет позволяет добиться эффекта постепенного затухания символов
+  $.fillStyle = 'rgba(0, 0, 0, .05)';
+
+  // заливаем холст выбранный цветом
+  $.fillRect(0, 0, W, H);
+
+  // меняем цвет для шрифта
+  $.fillStyle = '#0f0';
+
+  // устанавливаем параметры шрифта
+  $.font = font + 'px system-ui';
+
+  // рисуем символы
+  for (let i = 0; i < arr.length; i++) {
+    // выбираем случайный набор символов
+    let txt = matrix[Math.floor(Math.random() * matrix.length)];
+
+    // рисуем символы
+    // двигаемся вправо и вниз
+    // fillText(набор символов, координата x = значение i, умноженное на размер шрифта, координата y = значение arr, умноженное на размер шрифта)
+    $.fillText(txt, i * font, arr[i] * font);
+
+    // если "y" больше высоты холста или Math.random() выдает больше 0.975 (чем это значение меньше, тем больше будет пустот на экране), то поднимаемся наверх (обнуляем "y")
+    // это позволяет обеспечить разницу отрисовки отдельных колонок
+    if (arr[i] * font > H && Math.random() > 0.975) arr[i] = 0;
+
+    // увеличиваем значение y
+    arr[i]++;
+  }
 }
-
-function StopMatrix() {
-  clearInterval(Game_Interval);
-}
-setInterval(draw, 200);
+setInterval(draw, 80);
